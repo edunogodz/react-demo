225d4193d33289bd50b0e794ba382155fb1f2e2b
ceduardo <edunogodz@gmail.com>
2022-04-16 17:54:41 -0300

    Until: Parent child communication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
-import { Greet } from './components/Greet'
+//import { Greet } from './components/Greet'
+//import { Message } from './components/Message'
+//import { ClickHandler } from './components/ClickHandler'
+import { ParentComponent } from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Greet />
-      <Greet />
+
+      {/* <Greet name='Bruce' heroName='Batman' />
+      <Greet name='Clark' heroName='Superman'>
+        <button>Action</button>
+      </Greet>
+      <Greet name='Diana' heroName='Wonder Woman'>
+        <p>This is Children props</p>
+      </Greet> */}
+
+     {/* <Message /> */}
+
+      {/* <ClickHandler /> */}
+
+      <ParentComponent />
     </div>
   );
 }
